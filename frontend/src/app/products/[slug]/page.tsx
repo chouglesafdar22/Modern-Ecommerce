7ff5bdd0126e2417791c0fabc6b7093a6ae3c422
Safdar Chougle <chouglesafdar22@gmail.com>
2025-11-23 13:25:43 +0530

    update while deployment

--- frontend/src/app/products/[slug]/page.tsx
+++ frontend/src/app/products/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import Not_found from "@/app/not-found";
 import ProductDetails from "./ProductDetailsPage";
+import { notFound } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 interface ProductDetailPageProps {
   params: {
@@ -8,10 +11,16 @@ interface ProductDetailPageProps {
 }
 
 export default async function ProductDetailPage({ params }: ProductDetailPageProps) {
-  const { slug } = await params;
+  const { slug } = params;
   const id = slug.split("-").pop();
+
   try {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) return notFound();
+
     const product = await res.json();
     return <ProductDetails product={product} />;
   } catch (error) {
