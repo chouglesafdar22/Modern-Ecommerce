3ba5e8814347b27c3899018a757f9c1b814dd9d2
Safdar Chougle <chouglesafdar22@gmail.com>
2025-11-29 23:58:08 +0530

    changed mongoDb local to mongoDb atlas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.PUBLIC_URL,
     credentials: true
 }));
 app.use(express.json());
