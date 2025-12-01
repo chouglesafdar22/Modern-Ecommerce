6369fb0d2b58c1b92064ff2c9908752c0568b60c
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-01 17:01:23 +0530

    changes in ui

--- frontend/src/app/components/ProductCard.tsx
+++ frontend/src/app/components/ProductCard.tsx
@@ -21,7 +21,7 @@ function ProductCard({ product }: any) {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ type: "spring", duration: 0.4, ease: "anticipate" }}
-            className="group flex flex-col xl:gap-4 lg:gap-3 md:gap-2.5 sm:gap-1.5 gap-0.5 items-center rounded-xl xl:py-3.5 lg:py-3 md:py-2.5 sm:py-2 py-1.5 lg:px-3.5 md:px-2.5 sm:px-1.5 px-1 w-[235px] xl:w-[260px] max:xl:h-[400px] max:h-[360px] bg-gray-100 font-sans shadow-gray-300 shadow-md"
+            className="group flex flex-col xl:gap-4 lg:gap-3 md:gap-2.5 sm:gap-1.5 gap-0.5 items-center rounded-xl xl:py-3.5 lg:py-3 md:py-2.5 sm:py-2 py-1.5 lg:px-3.5 md:px-2.5 sm:px-1.5 px-1 w-[260px] xl:w-[260px] max:xl:h-[400px] max:h-[400px] bg-gray-100 font-sans shadow-gray-300 shadow-md"
         >
             {/* Image Box */}
             <motion.div
@@ -31,8 +31,8 @@ function ProductCard({ product }: any) {
                 <Image
                     src={product.image}
                     alt={product.name}
-                    width={100}
-                    height={100}
+                    width={150}
+                    height={150}
                     loading='eager'
                     unoptimized
                     className="object-contain py-4 lg:w-[200px] cursor-pointer lg:h-[200px]"
@@ -53,27 +53,27 @@ function ProductCard({ product }: any) {
                         </span>
                     )}
                 </div>
-                <h4 className="text-black xl:text-xl lg:text-lg sm:text-base text-sm font-medium line-clamp-2">
+                <h4 className="text-black xl:text-2xl lg:text-xl sm:text-lg text-base font-medium line-clamp-2">
                     {product.name}
                 </h4>
                 <span className='flex flex-1'>
-                    <p className="text-black xl:text-lg lg:text-base sm:text-sm text-[12px] font-light line-through">
+                    <p className="text-black xl:text-xl lg:text-lg sm:text-base text-sm font-light line-through">
                         ₹{product.price}
-                    </p>/<p className="text-black xl:text-lg lg:text-base sm:text-sm text-[12px] font-medium">
+                    </p>/<p className="text-black xl:text-xl lg:text-lg sm:text-base text-sm font-medium">
                         ₹{product.discountPrice}
                     </p>
                 </span>
                 {/* Rating Section */}
                 <div className="flex flex-row gap-1.5 items-center">
                     <div className="flex flex-row gap-1 justify-center items-center text-center">
-                        <p className="flex text-left xl:text-base lg:text-sm sm:text-[12px] text-[10px] font-light gap-1">
+                        <p className="flex text-left xl:text-lg lg:text-base sm:text-sm text-xs font-light gap-1">
                             {product.rating.toFixed(1)}
                         </p>
-                        <span className="flex justify-center text-left xl:text-lg lg:text-base sm:text-sm text-[12px] font-light">
+                        <span className="flex justify-center text-left xl:text-xl lg:text-lg sm:text-base text-sm font-light">
                             <StarRating rating={product.rating} />
                         </span>
                     </div>
-                    <p className="flex text-left xl:text-[17px] lg:text-[15px] sm:text-[13px] text-[11px] font-light gap-1">
+                    <p className="flex text-left xl:text-[19px] lg:text-[17px] sm:text-[15px] text-[13px] font-light gap-1">
                         ({product.numReviews})
                     </p>
                 </div>
