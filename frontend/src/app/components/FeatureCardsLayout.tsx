49dc7f0c91cad8d53ccd9a9461dfa97f91b4a502
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-03 14:33:37 +0530

    changes in featurecard

--- frontend/src/app/components/FeatureCardsLayout.tsx
+++ frontend/src/app/components/FeatureCardsLayout.tsx
@@ -8,7 +8,7 @@ export default function FeaturesCardLayout() {
     return (
         <section className="featuresCardSection xl:px-7 lg:px-5 md:px-3 sm:px-1 px-0.5 pt-8 pb-5">
             <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-center text-center items-center gap-5 rounded-xl bg-gray-100 py-5 px-0.5">
-                <FeatureCard icon={<PiTruck />} heading={"Free Shipping"} text={"Order Above ₹399"} />
+                <FeatureCard icon={<PiTruck />} heading={"Fast Delivery"} text={"Delivery in 4-7 days"} />
                 <FeatureCard icon={<PiMoney />} heading={"Money Refund"} text={"2 Days Guarantee"} />
                 <FeatureCard icon={<BsTelephone />} heading={"Premium Support"} text={"Phone & Email Support"} />
                 <FeatureCard icon={<SlLock />} heading={"Secure Payments"} text={"Secured by Stripe"} />
